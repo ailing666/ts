e1d6e5273f0902526237c8ad124df16f03e141bd
TENCENT\v_llingai <1120877019@qq.com>
2021-07-30 14:18:22 +0800

    food类完成

--- snakeGame/src/index.ts
+++ snakeGame/src/index.ts
@@ -1,3 +1,10 @@
 // 引入样式
 import './style/index.less'
+import Food from './modules/Food';
 
+
+// 测试代码
+const food = new Food();
+console.log(food.X, food.Y);
+food.change();
+console.log(food.X, food.Y);
